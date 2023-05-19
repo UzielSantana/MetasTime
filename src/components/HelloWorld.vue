23277920518bbf7ee2f99836f71d90a1aa81e6e9
UzielSantana <uzzsantana@gmail.com>
2023-05-19 13:49:14 -0300

    projeto inicial

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,65 +1,285 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript" target="_blank" rel="noopener">typescript</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <div>
+      <div class="chart">
+        <div class="wrapper">
+          <div class="sidebar">
+            <!-- conteúdo da barra lateral -->
+          </div>
+          <div class="main-content">
+            <!-- conteúdo principal -->
+          </div>
+        </div>
+        <canvas id="myChart"></canvas>
+      </div>
+    </div>
+
+    <div>
+      <div class="grafico1">
+        <canvas id="myChart2"></canvas>
+      </div>
+
+      <h1 class="h12">Destravamentos - Ativados - Travados - Desaparecidos</h1>
+      <div>
+        <canvas id="myChart3"></canvas>
+      </div>
+    </div>
+
+    <div>
+      <div class="sidebar">
+        <Sidebar @update-chart-individual="updateChartIndividual" @update-chart-coletivo="updateChartColetivo" @update-chart-coletivo2="updateChartColetivo2"></Sidebar>
+      </div>
+      <div class="chart">
+        <canvas id="myChart"></canvas>
+      </div>
+      <br>
+      <h1>Essas são as metas individuais de destravamentos no Onboarding Enterprise</h1>
+    </div>
   </div>
 </template>
 
-<script lang="ts">
-import { Options, Vue } from 'vue-class-component';
+<script>
+import Chart from 'chart.js/auto';
+import Sidebar from './SideBar.vue';
 
-@Options({
-  props: {
-    msg: String
-  }
-})
-export default class HelloWorld extends Vue {
-  msg!: string
-}
+export default {
+  components: {
+    Sidebar,
+  },
+  mounted() {
+    this.drawChart();
+    
+  },
+  methods: {
+    drawChart() {
+      const ctx = document.getElementById('myChart');
+      const ctx2 = document.getElementById('myChart2');
+      const ctx3 = document.getElementById('myChart3');
+
+      this.chart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+          labels: ['Bia', 'Carol', 'Dani', 'Nath', 'Thay', 'Uzi'],
+          datasets: [
+            {
+              label: 'Meta Realizada',
+              data: [1, 5, 1, 3, 3, 2, 5],
+              backgroundColor: '#FF6384', // cor da barra para 'Meta Realizada'
+              borderWidth: 1,
+              width: 20,
+            },
+            {
+              label: 'Meta Não Realizada',
+              data: [4, 2, 4, 2, 2, 3, 4],
+              backgroundColor: '#36A2EB', // cor da barra para 'Meta Não Realizada'
+              borderWidth: 1,
+              width: 20,
+            },
+          ],
+        },
+        options: {
+          layout: {
+            padding: 90,
+          },
+          scales: {
+            y: {
+              beginAtZero: true,
+            },
+          },
+        },
+      });
+
+      this.chart2 = new Chart(ctx2, {
+        type: 'line',
+        data: {
+          labels: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho'],
+          datasets: [
+            {
+              label: 'Travados',
+              data: [12, 19, 3, 5, 2, 3],
+              fill: 'origin',
+            },
+            {
+              label: 'Destravados',
+              data: [3, 5, 7, 1, 0, 6],
+              fill: '+2',
+            },
+            {
+              label: 'Em Onboarding',
+              data: [1, 5, 2, 5, 2, 5],
+              fill: 1,
+            },
+            {
+              label: 'Sem próximos passos',
+              data: [4, 6, 1, 4, 2, 7],
+              fill: false,
+            },
+          ],
+        },
+        options: {
+          scales: {
+            y: {
+              beginAtZero: true,
+            },
+          },
+        },
+        updateChart(data) {   
+
+      
+
+      this.chart2.update();
+      console.log(data);
+    }
+      });
+
+      this.chart3 = new Chart(ctx3, {
+              type: 'doughnut',
+              data: {
+                labels: ['Travados', 'Destravados', 'Em Onboarding', 'Sem próximos passos'],
+                datasets: [
+                  {
+                    label: 'Quantidade',
+                    data: [12, 7, 5, 10],
+                    backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56', '#4BC0C0'],
+                    borderWidth: 1,
+                    width: 20,
+                  },
+                ],
+              },
+              options: {
+                cutout: '60%', // Controla o tamanho do círculo central vazado (de 0 a 100)
+                plugins: {
+                  legend: {
+                    display: true,
+                    position: 'right', // Posição da legenda (pode ser 'top', 'bottom', 'left', 'right')
+                  },
+                },
+              },
+            });
+        
+      
+      this.chart3.update();
+    },
+    updateChartIndividual(data) {
+      this.chart.data.datasets[0].data = [
+        data.data1,
+        data.data3,
+        data.data5,
+        data.data7,
+        data.data9,
+        data.data11,
+      ];
+      this.chart.data.datasets[1].data = [
+        data.data2,
+        data.data4,
+        data.data6,
+        data.data8,
+        data.data10,
+        data.data12,
+      ];
+      this.chart.update();
+    },
+    updateChartColetivo(data) {
+      this.chart2.data.datasets[0].data = [
+        data.data13,
+        data.data14,
+        data.data15,
+        data.data16,
+      ];
+      this.chart2.update();
+    },
+    updateChartColetivo2(data) {
+      this.chart3.data.datasets[0].data = [
+        data.data17,
+        data.data18,
+        data.data19,
+        data.data20,
+      ];
+     
+      
+      
+      this.chart3.update();
+    },
+  },
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+<style >
+
+
+
+.wrapper {
+  display: flex;
+  height: 100%;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.sidebar {
+  margin:0;
+  width: 200px;
+  background-color: #ccc;
+  padding: 20px;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+.main-content {
+  flex: 1;
+  background-color: #fff;
+  padding: 20px;
 }
-a {
-  color: #42b983;
+
+
+
+body {
+
+
+  background-color: rgb(223, 214, 214);
+  
+}
+
+img {
+
+  margin-left: 50px;
+  margin-right: -50px;
+  margin-top: 30px
+  wi
+  
+}
+
+h1 { 
+
+  margin-left: -80px ;
+  margin-top: 40px;
+  margin-bottom:-50px;
+  padding-bottom: - 70px;
+
+  color:rgb(222, 222, 35);
+  font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+}
+
+
+.h12 { 
+
+margin-left: -80px ;
+margin-top:40px;
+margin-bottom:-50px;
+padding-bottom: - 70px;
+color:rgb(222, 222, 35);
+
+font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+}
+
+
+.grafico {
+
+  max-width: 200px;
+}
+
+canvas {
+
+  width: 100px;
+}
+
+.imgs {
+
+  display: flex;
 }
 </style>
---
+++ src/components/SideBar.vue
@@ -0,0 +1,127 @@
+<template>
+  <div>
+    <h2>Atualizador do gráfico Individual</h2>
+    <form>
+      <label for="data1">Meta Bia:</label>
+      <input type="number" id="data1" v-model="data1">
+      <label for="data2">Meta Atingida Bia:</label>
+      <input type="number" id="data2" v-model="data2">
+      <label for="data3">Meta Carol:</label>
+      <input type="number" id="data3" v-model="data3">
+      <label for="data4">Meta Atingida Carol:</label>
+      <input type="number" id="data4" v-model="data4">
+      <label for="data5">Meta Dani:</label>
+      <input type="number" id="data5" v-model="data5">
+      <label for="data6">Meta Atingida Dani:</label>
+      <input type="number" id="data6" v-model="data6">
+      <label for="data7">Meta Nath:</label>
+      <input type="number" id="data7" v-model="data7">
+      <label for="data8">Meta Atingida Nath:</label>
+      <input type="number" id="data8" v-model="data8">
+      <label for="data9">Meta Thay:</label>
+      <input type="number" id="data9" v-model="data9">
+      <label for="data10">Meta Atingida Thay:</label>
+      <input type="number" id="data10" v-model="data10">
+      <label for="dat11">Meta Uzi:</label>
+      <input type="number" id="data11" v-model="data11">
+      <label for="data12">Meta Atingida Uzi:</label>
+      <input type="number" id="data12" v-model="data12">
+
+      <button type="button" @click="updateChart">Atualizar</button>
+    </form>
+
+    <h2>Atualizador do gráfico Coletivo</h2>
+    <form>
+      <label for="data13">Travados:</label>
+      <input type="number" id="data13" v-model="data11">
+      <label for="data14">Destravados:</label>
+      <input type="number" id="data14" v-model="data12">
+      <label for="data15">Clientes em Onboarding:</label>
+      <input type="number" id="data13" v-model="data13">
+      <label for="data14">Clientes sem próximos passos:</label>
+      <input type="number" id="data14" v-model="data14">
+
+      <button type="button" @click="updateChartColetivo">Atualizar</button>
+    </form>
+
+    <h2>Atualizador do gráfico Coletivo2</h2>
+    <form>
+      <label for="data17">Meta1:</label>
+      <input type="number" id="data17" v-model="data17">
+      <label for="data18"> Meta2: </label>
+      <input type="number" id="data18" v-model="data18">
+      <label for="data19"> Meta 3:</label>
+      <input type="number" id="data19" v-model="data19">
+      <label for="data20"> Meta4: </label>
+      <input type="number" id="data20" v-model="data20">
+
+      <button type="button" @click="updateChartColetivo2">Atualizar</button>
+    </form>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  data() {
+    return {
+      data1: 0,
+      data2: 0,
+      data3: 0,
+      data4: 0,
+      data5: 0,
+      data6: 0,
+      data7: 0,
+      data8: 0,
+      data9: 0,
+      data10: 0,
+      data11: 0,
+      data12: 0,
+      data13: 0,
+      data14: 0,
+      data15: 0,
+      data16: 0,
+      data17: 0,
+      data18: 0,
+      data19:0,
+      data20:0
+    }
+  },
+  methods: {
+   
+    updateChart() {
+      this.$emit('update-chart-individual', {
+        data1: this.data1,
+        data2: this.data2,
+        data3: this.data3,
+        data4: this.data4,
+        data5: this.data5,
+        data6: this.data6,
+        data7: this.data7,
+        data8: this.data8,
+        data9: this.data9,
+        data10: this.data10,
+        data11: this.data11,
+        data12: this.data12
+      });
+    },
+    updateChartColetivo() {
+      this.$emit('update-chart-coletivo', {
+        data13: this.data13,
+        data14: this.data14,
+        data15: this.data15,
+        data16: this.data16,
+      });
+    },
+    updateChartColetivo2() {
+      this.$emit('update-chart-coletivo2', {
+        data17: this.data17,
+        data18: this.data18,
+        data19: this.data19,
+        data20: this.data20,
+      });
+    },
+  },
+};
+</script>
